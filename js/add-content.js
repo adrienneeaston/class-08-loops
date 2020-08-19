a40b845f579b5d136a49218b47efcb57c12015a2
Adrienne <adrienneeaston@gmail.com>
2020-08-18 20:53:29 -0700

    after demo

--- js/add-content.js
+++ js/add-content.js
@@ -21,22 +21,51 @@ function getGreeting() {
   return document.write('<h3>' + greeting + '</h3>');
 }
 
+// helper function
 function getPreference() {
   var response = prompt('Would you like a house or a hotel?');
   var item;
+
+  // true && true
+  while(response != 'house' && response != 'hotel') {
+    response = prompt('PLEASE answer correctly! Would you like a house or hotel?');
+  }
   
   if (response === 'house') {
     item = '<img src ="images/house.png">';
   } else if (response === 'hotel') {
     item = '<img src ="images/hotel.png">';
-  } else {
-    item = 'No house or hotel for you!';
-  }
+  } 
   
-  return document.write('<p>' + item + '</p>');
+  return item;
+  // return document.write('<p>' + item + '</p>');
+}
+
+// Helper function
+function getCount() {
+  var count = prompt('Great! How many would you like?');
+  // true/false
+  while(isNaN(count) || count === '') {
+    count = prompt('PLEASE enter a number! How many would you like?');
+  }
+  return count;
+}
+
+function showOrder() {
+  var result = '';
+  var itemType = getPreference();
+  var total = getCount();
+
+  for(var i = 0; i < total; i++) {
+    result = result + '<p>' + itemType + '</p>';
+  }
+
+  return document.write(result);
 }
 
 getName();
 getGreeting();
-getPreference();
+// getPreference();
+showOrder();
+
 
